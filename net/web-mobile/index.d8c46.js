173a0fcac8bb8425f0d6a36cbdf8cc60ae9cb839
842673413@qq.com <842673413@qq.com>
2025-09-17 16:36:07 +0800

    Site updated: 2025-09-17 16:36:05

--- net/web-mobile/index.d8c46.js
+++ net/web-mobile/index.d8c46.js
@@ -1,4 +1,4 @@
-System.register(["./application.2f2ab.js"], function (_export, _context) {
+System.register(["./application.3a788.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.2f2ab.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application2f2abJs) {
-      Application = _application2f2abJs.Application;
+    setters: [function (_application3a788Js) {
+      Application = _application3a788Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
