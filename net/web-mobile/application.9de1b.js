942b50109a3c4de340f04c8d4d8274a4d58103a8
842673413@qq.com <842673413@qq.com>
2025-09-17 19:28:28 +0800

    Site updated: 2025-09-17 19:28:26

--- net/web-mobile/application.9de1b.js
+++ net/web-mobile/application.9de1b.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.c7a11.json';
+          this.settingsPath = 'src/settings.22bfd.json';
           this.showFPS = false;
         }
 
