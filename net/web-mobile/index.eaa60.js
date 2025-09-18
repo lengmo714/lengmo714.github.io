a4c92465e66114935235378d51e7eed582968ca9
842673413@qq.com <842673413@qq.com>
2025-09-18 12:12:24 +0800

    Site updated: 2025-09-18 12:12:21

--- net/web-mobile/index.eaa60.js
+++ net/web-mobile/index.eaa60.js
@@ -1,4 +1,4 @@
-System.register(["./application.5095b.js"], function (_export, _context) {
+System.register(["./application.5b7c1.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.5095b.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application5095bJs) {
-      Application = _application5095bJs.Application;
+    setters: [function (_application5b7c1Js) {
+      Application = _application5b7c1Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
