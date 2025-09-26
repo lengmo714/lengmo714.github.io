d3b792b177978faa18f72254f8bc5913fa672f41
842673413@qq.com <842673413@qq.com>
2025-09-26 10:39:29 +0800

    Site updated: 2025-09-26 10:39:21

--- net/web-mobile3/index.072f6.js
+++ net/web-mobile3/index.072f6.js
@@ -1,4 +1,4 @@
-System.register(["./application.bcbd0.js"], function (_export, _context) {
+System.register(["./application.0fd2c.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.bcbd0.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationBcbd0Js) {
-      Application = _applicationBcbd0Js.Application;
+    setters: [function (_application0fd2cJs) {
+      Application = _application0fd2cJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
