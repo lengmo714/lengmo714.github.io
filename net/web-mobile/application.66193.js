b889784dc9962c2c6dab869219a8638d46f61b4d
842673413@qq.com <842673413@qq.com>
2025-10-31 21:00:54 +0800

    Site updated: 2025-10-31 21:00:48

--- net/web-mobile/application.66193.js
+++ net/web-mobile/application.66193.js
@@ -16,7 +16,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.877a7.json';
+          this.settingsPath = 'src/settings.12544.json';
           this.showFPS = false;
         }
 
--- net/web-mobile/index.3e795.js
+++ net/web-mobile/index.3e795.js
@@ -1,4 +1,4 @@
-System.register(["./application.a6eaf.js"], function (_export, _context) {
+System.register(["./application.66193.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.a6eaf.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationA6eafJs) {
-      Application = _applicationA6eafJs.Application;
+    setters: [function (_application66193Js) {
+      Application = _application66193Js.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
