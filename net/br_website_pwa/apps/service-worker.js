5b6dd539119de9597816e90bf7d958c69d802ff5
842673413@qq.com <842673413@qq.com>
2025-11-07 11:01:51 +0800

    Site updated: 2025-11-07 11:01:50

--- net/br_website_pwa/apps/service-worker.js
+++ net/br_website_pwa/apps/service-worker.js
@@ -1,10 +1,12 @@
 self.addEventListener('install', (event) => {
     console.log("Service Worker 已安装");
     self.skipWaiting();
+    event.waitUntil(Promise.resolve());
 });
 
 self.addEventListener('activate', (event) => {
     console.log("Service Worker 已激活");
+    event.waitUntil(clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
