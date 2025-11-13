3133a7fc8ef3c50ca14745d557f311d7a349a1e0
842673413@qq.com <842673413@qq.com>
2025-11-13 14:54:24 +0800

    Site updated: 2025-11-13 14:54:20

--- net/web-mobile1/index.0c0ea.js
+++ net/web-mobile1/index.0c0ea.js
@@ -1,4 +1,4 @@
-System.register(["./application.b4983.js"], function (_export, _context) {
+System.register(["./application.260cf.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.b4983.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_applicationB4983Js) {
-      Application = _applicationB4983Js.Application;
+    setters: [function (_application260cfJs) {
+      Application = _application260cfJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
