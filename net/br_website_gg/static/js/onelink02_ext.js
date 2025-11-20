1ea106627c84e0c607915ea2ea07def9218e4bbb
842673413@qq.com <842673413@qq.com>
2025-11-20 16:29:39 +0800

    Site updated: 2025-11-20 16:29:36

--- net/br_website_gg/static/js/onelink02_ext.js
+++ net/br_website_gg/static/js/onelink02_ext.js
@@ -42,7 +42,6 @@ class onelink02 {
                 });
                 var result_url = null;
                 if (afRes != null) {
-                    console.log("111--- dstOnelink2:", afRes.clickURL);
                     result_url = afRes.clickURL +`&uuid=${Date.now()}${Math.floor(Math.random() * 100000)}`;
                     console.log("--- result_url:",result_url);
                     if (gaid)
